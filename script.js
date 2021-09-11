1a71a03fba2f139ffeb9f22f50f9665915e234c4
TrangTheDev <trangh1@hotmail.com>
2021-09-11 12:28:09 +1000

    Add JS questions

---
+++ script.js
@@ -0,0 +1,80 @@
+
+var questions = [
+    {   question : "Which element is used to create a heading?",
+        answer1 : "<h1>",
+        answer2 : "<p>",
+        answer3 : "<img>",
+        answer4 : "<div>"
+    },
+    {
+        question : "Which one of these is not an ID",
+        answer1 : "#element",
+        answer2 : "#left",
+        answer3 : ".right",
+        answer4 : "#top",
+    },
+    {
+        question : "Media query does what?",
+        answer1 : "Gives you information about latest technology",
+        answer2 : "A CSS technique which allows different presentations",
+        answer3 : "A Java script command",
+        answer4 : "Is an element in html",
+    },
+    {
+        question : "When a user enters their information in a form and it is stored on their local, where is it stored?",
+        answer1 : "Local storage",
+        answer2 : "In the database",
+        answer3 : "On the cookies",
+        answer4 : "In the HTML",
+    },
+    {
+        question : "What is the command to convert object to a string?", 
+        answer1 : "JSON.parse",
+        answer2 : "localStorage.setItem",
+        answer3 : "JSON.stringify",
+        answer4 : "addEventListener",
+    },
+    {
+        question : "What is the terminal command to copy a repo from git to your local?",
+        answer1 : "cd",
+        answer2 : "init",
+        answer3 : "clone",
+        answer4 : "touch",
+    },
+    {
+        question : "What is a Boolean",
+        answer1 : "a number value",
+        answer2 : "a string value",
+        answer3 : "a decimal value",
+        answer4 : "a true or false value",
+    },
+    {
+        question : "What is this operator ||",
+        answer1 : "or",
+        answer2 : "and",
+        answer3 : "add",
+        answer4 : "sum",
+    },
+    {
+        question : "(2,4,6,8,10,12,14,16,18,20) What is the index value of 10",
+        answer1 : "5",
+        answer2 : "6",
+        answer3 : "10",
+        answer4 : "4",
+    },
+    {
+        question : "What is an array?",
+        answer1 : "A line of code that points to a link",
+        answer2 : "A css style to create a table",
+        answer3 : "A command to start a script",
+        answer4 : "A collection of the same data type",
+    },
+
+    
+
+
+
+
+
+
+]
